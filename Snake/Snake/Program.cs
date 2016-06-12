b3d01800c01c6b43fc90ce8a08a6cc9e7fb4903b
Yakov-Podskarbiy <pjn92@mail.ru>
2016-06-12 14:08:05 +0700

    второе занятие-функции

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,29 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = 50;
+
+            for (int i = 10; i <= n; i++)
+            {
+                int x1 = i;
+                int y1 = 0;
+                char s1 = '*';
+
+                wr(x1, y1, '*');
+
+            }
+
             Console.ReadLine();
+            
+           }
+
+       static void wr(int x, int y, char s)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(s);
+          
+          
+
         }
     }
 }
